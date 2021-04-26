e8bc8a6772594780f98a0f5bc1f87b5a4dd344a6
Lucy Mao <>
2021-04-26 23:39:03 +0000

    review 3 done

--- src/gr11review/part1/Review3.java
+++ src/gr11review/part1/Review3.java
@@ -4,6 +4,15 @@ import java.io.*;
 
 public class Review3{
 	public static void main(String[] args) throws IOException{
-    
+
+    for(int i = 20;i<100;i++){
+      if(i %2 != 0){
+        System.out.println(i);
+      }
+    }
+
+    for(int i = 29; i>=2;i--){
+      System.out.println(i);
+    }
   }
 }
