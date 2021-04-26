ec40daeb084989c7df88973dd71db7a214026324
Lucy Mao <>
2021-04-26 22:46:34 +0000

    first and second mostly done

--- src/gr11review/part1/Review1.java
+++ src/gr11review/part1/Review1.java
@@ -1,1 +1,32 @@
 package gr11review.part1;
+
+import java.io.*;
+
+public class Review1{
+	public static void main(String[] args) throws IOException{
+
+		int intMonth = 0;
+    int intDay = 0;
+    int intMonthNum = 0;
+
+    int[] intMonthsList = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+
+    System.out.print("Enter the month number: ");
+    intMonth = Integer.parseInt(keyboard.readLine());
+
+    System.out.print("Enter the day number: ");
+    intDay = Integer.parseInt(keyboard.readLine());
+
+    for(int i = 0; i<=12; i++){
+      if(i == intMonth){
+        for(int s = 1; s<i; s++){
+          intMonthNum += intMonthsList[s-1];
+        }
+      }
+    }
+    System.out.println(intMonthNum+intDay);
+
+
+  }
+}
--- src/gr11review/part1/Review2.java
+++ src/gr11review/part1/Review2.java
@@ -1,1 +1,31 @@
 package gr11review.part1;
+
+import java.io.*;
+
+public class Review2{
+	public static void main(String[] args) throws IOException{
+    BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+
+    String strJoke = "";
+
+    System.out.println("0 - print an joke about your hair");
+    System.out.println("1 - print a joke about your feet");
+    System.out.println("2 - print a joke about your clothes"); 
+    System.out.println("3 - print a joke about your teacher");
+    System.out.print("Choose a menu option: ");
+
+    int intChoice = Integer.parseInt(keyboard.readLine());
+
+    switch (intChoice){
+      case 1: strJoke = "What do you call Italian cheese with curly hair? Perm-esan.";
+      break;
+      case 2: strJoke = "My boyfriend told me to stop acting like a flamingo. So I had to put my foot down.";
+      break;
+      case 3: strJoke = "What is DNA’s favourite piece of clothing? Jeans";
+      break;
+      default: strJoke = "Invalid menu option";
+      break;
+    }
+    System.out.println(strJoke);
+  }
+}
