f4a3d7606f0681766dcf34adc40e4fe7e08fe15e
Lucy Mao <>
2021-04-27 18:26:42 +0000

    Finished 5

--- src/gr11review/part1/Review5.java
+++ src/gr11review/part1/Review5.java
@@ -1,1 +1,35 @@
 package gr11review.part1;
+import java.io.*;
+
+public class Review5{
+	public static void main(String[] args) throws IOException{
+    
+    BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+
+    double yearly_amount = 0;
+    double compound_interest_rate = 0;
+    double target_amount = 0;
+
+    System.out.print("Enter the yearly invested amount: ");
+    yearly_amount = Double.parseDouble(keyboard.readLine());
+
+    System.out.print("Enter the compound interest rate: ");
+    compound_interest_rate = Double.parseDouble(keyboard.readLine());
+
+    System.out.print("Enter the target amount: ");
+    target_amount = Double.parseDouble(keyboard.readLine());
+
+    int i = 0;
+    double compound_amount = 0;
+    double total_money = 0;
+
+    while (total_money < target_amount){
+      
+      compound_amount = ((total_money + yearly_amount)*(compound_interest_rate/100));
+
+      total_money += compound_amount+yearly_amount;
+      i++;
+    }
+    System.out.println("The target amount will be earned in " + i + " years.");
+  }
+}
