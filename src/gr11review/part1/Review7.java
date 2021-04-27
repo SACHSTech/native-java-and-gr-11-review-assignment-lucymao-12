5b45381ada084e2aa8b6ffe69ff4471aa1f28782
Lucy Mao <>
2021-04-27 23:58:08 +0000

    7 finished

--- src/gr11review/part1/Review7.java
+++ src/gr11review/part1/Review7.java
@@ -1,1 +1,33 @@
 package gr11review.part1;
+import java.io.*;
+
+public class Review7{
+	public static void main(String[] args) throws IOException{
+    
+    BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+
+    String theSentence = keyboard.readLine();
+    int[] charArray = {0, 0 , 0};
+    String newString = "";
+
+    for(int i = 0; i < theSentence.length(); i++){
+        charArray[0]++;
+      
+      if(theSentence.charAt(i) == ' '){
+        charArray[1]++;
+      }
+      if(theSentence.charAt(i) == 'a'){
+        charArray[2]++;
+      }
+      if(i % 2 == 0){
+        newString += "-";
+      }
+    }
+    
+    System.out.println("There are " + charArray[0] + " characters in the sentence.");
+    System.out.println("There are "+charArray[1]+" spaces in the sentence.");
+    System.out.println("There are "+charArray[2]+" letter a in the sentence.");
+    System.out.println(newString);
+    
+  }
+}
--- src/gr11review/part1/Review8.java
+++ src/gr11review/part1/Review8.java
@@ -1,1 +1,25 @@
 package gr11review.part1;
+import java.lang.Math;
+import java.io.*;
+
+public class Review8{
+	public static void main(String[] args) throws IOException{
+    
+    int[] slot_numbers = {0, 0, 0};
+    int s = 0;
+
+    for(int i = 0; i < 1000; i++){
+      slot_numbers[0] = (int)(Math.random()*10);
+      slot_numbers[1] = (int)(Math.random()*10);
+      slot_numbers[2] = (int)(Math.random()*10);
+      System.out.print(slot_numbers[0]+ " ");
+      System.out.print(slot_numbers[1]+" ");
+      System.out.println(slot_numbers[2]);
+      
+      if(slot_numbers[0] == slot_numbers[1] && slot_numbers[0] == slot_numbers[2]){
+        s++;
+      }
+    }
+    System.out.println(s);
+  }
+}
